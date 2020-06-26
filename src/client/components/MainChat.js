86409c5d80f96513822a7a6ce5a9e88d4cd0d5f5
Midori Yang <myang5@wellesley.edu>
2020-06-26 13:07:56 -0400

    added UI skeleton with minimal React Router functionality

---
+++ src/client/components/MainChat.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+export default function MainChat() {
+  return (
+    <div className='mainContainer mainChat'>
+      <img />
+      <button>Log out of GitHub</button>
+      <button>Log out of Anon ID</button>
+      <p className='displayName'>Pikachu</p>
+      <div className='chat'>
+        <p>hello world</p>
+      </div>
+      <div className='inputArea'>
+        <input type='text'></input>
+        <button>Send</button>
+      </div>
+    </div>
+  )
+}
