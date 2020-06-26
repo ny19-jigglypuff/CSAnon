0842fbb1f975535a8fe5d92629f8b2aa03855647
VicBal <balanvic11235@gmail.com>
2020-06-26 19:03:31 -0400

    sql set up 2

--- src/server/models/elephantsql.js
+++ src/server/models/elephantsql.js
@@ -1,18 +1,14 @@
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
 const connectionString = 'DB_URL'
 
 const pool = new Pool({
     connectionString: connectionString,
   })
-  pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
-  const client = new Client({
-    connectionString: connectionString,
-  })
-  client.connect()
-  client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
-  })
+  
+
+module.exports = {
+    query: (text, params, callback) => {
+      console.log('executed query', text);
+      return pool.query(text, params, callback);
+},
+}
