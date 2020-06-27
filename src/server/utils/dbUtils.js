35cf8476027bc8b3c8d5641daaeb7956d22e5e0b
Steve Budarz <sbudarz@gmail.com>
2020-06-27 12:10:58 -0400

    messages should be forwarded and saved to db

---
+++ src/server/utils/dbUtils.js
@@ -0,0 +1,32 @@
+const db = require('../models/elephantsql');
+
+const dbUtils = {};
+
+dbUtils.matchUsernameToID = async (username) => {
+  const queryString = `SELECT user_id FROM users WHERE username = "${username}"`;
+  const response = await db.query(queryString);
+  if (response.rows.length) {
+    const { userID } = response.rows[0];
+    return userID;
+  } else {
+    console.error('user id not found when searched by name');
+  }
+};
+
+dbUtils.getIDAndPictureByUsername = async (username) => {
+  const queryString = `SELECT pic_url, user_id FROM users WHERE username = ${username}`;
+  const result = await db.query(queryString);
+  if (result.rows.length) {
+    const { user_id, pic_url } = result.rows[0];
+    return { user_id, userURL: pic_url };
+  } else {
+    console.error('no name and picture results found');
+  }
+};
+
+dbUtils.saveMessageToDB = async ({ message, user_id }) => {
+  const queryString = `INSERT INTO messages (user_id, message) VALUES ('${user_id}', '${message}') `;
+  return await db.query(queryString);
+};
+
+module.exports = dbUtils;
--- src/server/ws/ws.js
+++ src/server/ws/ws.js
@@ -1,3 +1,9 @@
+const { getIDAndPictureByUsername, saveMessageToDB } = require('../utils/dbUtils');
+const moment = require('moment');
+const db = require('../models/elephantsql');
+const redis = require('../redis/redis');
+moment().format();
+
 module.exports = (http) => {
   const io = require('socket.io')(http);
 
@@ -7,10 +13,46 @@ module.exports = (http) => {
     // TODO: on message should do the following:
     //       1. save the message to database
     //       2. forward the message to all connected users
-    socket.on('message', console.log);
+    socket.on('message', async ({ message, username }) => {
+      const { user_id, userURL } = await getIDAndPictureByUsername(username);
+      const timestamp = moment();
+      const newMessage = { message: message, username, userURL, timestamp };
+      const dbMessage = { user_id, message };
+      try {
+        await saveMessageToDB(dbMessage);
+        io.emit('newMessage', newMessage);
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    socket.on('signin', (username) => {
+      redis.set(socket.id, username);
+      redis.set(username, true);
+    });
 
     // TODO: on disconnect, should free the user id from the redis database
-    socket.on('disconnect', () => console.log('user disconnected'));
+    socket.on('disconnect', () => {
+      const username = redis.get(socket.id);
+      console.log(username, 'disconnected');
+      redis.del(socket.id);
+      redis.del(username);
+    });
   });
   return io;
 };
+
+/* messageObject = {
+message: String,
+username:  String,
+userURL:  String,
+timestamp: String (format will be h:mm a)
+}
+*/
+
+/*
+  sendMessage = {
+  username: String,
+  message: String,
+  }
+ */
