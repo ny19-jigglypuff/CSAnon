52a11cf4f8d2f60175b57bf2b590b82e67976756
Midori Yang <myang5@wellesley.edu>
2020-06-27 13:58:42 -0400

    further styling

--- src/client/components/MainChat.js
+++ src/client/components/MainChat.js
@@ -17,6 +17,7 @@ export default function MainChat(props) {
 
   socket.on('newMessage', (data) => {
     //{username, userURL, timestamp, message}
+    console.log(data);
     const newArr = [...messageData];
     newArr.push(data);
     setMessageData(newArr);
@@ -25,28 +26,32 @@ export default function MainChat(props) {
   //TODO: add function for request more messages on scroll up
   if (messageData.length === 0) {
     fetch('/messages/all')
-    .then((res) => res.json())
-    .then((res) => {
-      setMessageData(res);
-    });
+      .then((res) => res.json())
+      .then((res) => {
+        setMessageData(res);
+      });
   }
 
   return (
     <div className='mainContainer mainChat'>
-      <img src={props.userURL} />
-      {/* TODO: add log out functionalities */}
-      <button>Log out of GitHub</button>
-      <button>Log out of Anon ID</button>
-      <p className='displayName'>Pikachu</p>
-      <div className='chat'>
-        {/* assumes most recent message is at the end of the array */}
-        {messageData.map((message) => (
-          <Message key={JSON.stringify(message)} {...message} />
-        ))}
+      <div className='sidebar'>
+        <p className='displayName'>Pikachu</p>
+        <img src={props.userURL} />
+        {/* TODO: add log out functionalities */}
+        <button>Log out of GitHub</button>
+        <button>Log out of Anon ID</button>
       </div>
-      <div className='inputArea'>
-        <input type='text' ref={inputMessageRef}></input>
-        <button onClick={handleSendClick}>Send</button>
+      <div className='chatContainer'>
+        <div className='chat'>
+          {/* assumes most recent message is at the end of the array */}
+          {messageData.map((message) => (
+            <Message key={JSON.stringify(message)} {...message} />
+          ))}
+        </div>
+        <div className='inputArea'>
+          <input type='text' ref={inputMessageRef}></input>
+          <button onClick={handleSendClick}>Send</button>
+        </div>
       </div>
     </div>
   );
@@ -56,7 +61,7 @@ function Message(props) {
   return (
     <div className='messageContainer'>
       <p>{props.timestamp}</p>
-      <img src={props.userURL} />
+      <img src={props.pic_url} />
       <p>{props.message}</p>
     </div>
   );
