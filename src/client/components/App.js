deab631f90110b379e7cd5797d04b386da645c63
Midori Yang <myang5@wellesley.edu>
2020-06-26 21:02:03 -0400

    set up websocket connection, initial data fetching, React Router

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -9,11 +9,11 @@ import MainChat from './MainChat';
 //if not loggedIn
   // redirect to /signin
 
-const loggedIn = true; //temporary
 
-//need more details about how OAuth returns values
 
 export default function App() {
+  const loggedIn = true; //TODO: check if jwt cookie exists or not
+  console.log('App.js: theoretically checking cookie');
   return (
     <BrowserRouter>
       <Switch>
@@ -24,15 +24,17 @@ export default function App() {
         {/* csanon.com/signin */}
         <Route path='/signin'>
           {/* show github oauth button */}
-          {/* if not successful signin -> server should send back some error */}
-          {/* if signin -> we set the the cookie, redirect to '/'? */}
+          {/* onclick, GET request on /auth endpoint to get auth URL */}
+          {/* user leaves page, goes to github to sign in */}
+          {/* successful signin -> goes back to server */}
+          {/* backend will redirect the user to the front end, with github username and JWT cookie*/}
+          {/* if approved CS user, backend will redirect the user to the front end on '/', with JWT cookie */}
+          {/* if not approved CS user -> server should send back some error */}
         </Route>
         {/* csanon.com/chat */}
-        <Route path='/chat'>
-          <MainChat />
+        <Route path='/chat' render={(props) => <MainChat {...props} />}>
         </Route>
       </Switch>
     </BrowserRouter>
-
   )
 }
